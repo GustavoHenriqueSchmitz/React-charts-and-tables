998a1b27668cbfce0ae5eac81ec7c2d1fd2abbc4
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-12-05 11:15:02 -0300

    back

--- back-end/controllers/employees.js
+++ back-end/controllers/employees.js
@@ -9,8 +9,8 @@ async function getEmployees(req, res) {
             'age',
             'jobFunction',
             'salary',
-            [Sequelize.fn('concat', 'R$ ', Sequelize.fn('format', Sequelize.col('salary'), 'pt_BR')), 'salary'],
-            [Sequelize.fn('concat', 'R$ ', Sequelize.fn('format', Sequelize.col('salary_target'), 'pt_BR')), 'salaryTarget'],
+            [Sequelize.fn('concat', Sequelize.fn('format', Sequelize.col('salary'), 'pt_BR')), 'salary'],
+            [Sequelize.fn('concat', Sequelize.fn('format', Sequelize.col('salary_target'), 'pt_BR')), 'salaryTarget'],
         ],
     })
 
@@ -26,8 +26,25 @@ async function getSalary(req, res) {
         ]
     })
 
-    const salaryList = []
-    salary.map( value => salaryList.push(value.salary))
+    const salaryList = [0, 0, 0, 0, 0]
+    salary.map( value => {
+        
+        if (value.salary > 0 && value.salary <= 500) {
+            salaryList[0] += 1
+        }
+        if (value.salary > 500 && value.salary <= 1000) {
+            salaryList[1] += 1
+        }
+        if (value.salary > 1000 && value.salary <= 2000) {
+            salaryList[2] += 1
+        }
+        if (value.salary > 2000 && value.salary <= 10000) {
+            salaryList[3] += 1
+        }
+        if (value.salary > 10000) {
+            salaryList[4] += 1
+        }
+    })
 
     res.send(salaryList)
     res.end()
